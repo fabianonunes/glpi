2d223f599957efe86deed683c360c25d412c117f
Fabiano Parente <fabiano@nunes.me>
2024-07-03 10:22:14 -0300

    ignora erro se install não existir

--- fs/init.sh
+++ fs/init.sh
@@ -16,7 +16,7 @@ php bin/console db:install                   \
   --default-language "${DEFAULT_LANGUAGE:?}" \
 || true
 
-rm install/install.php
+rm -f install/install.php
 
 php bin/console database:enable_timezones || true
 php bin/console db:update --no-interaction --no-telemetry
